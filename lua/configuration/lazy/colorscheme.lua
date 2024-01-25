7f7f4dc4ec2ec3754343bf7fabef72eee9ee4f76
Abhay Patel <apatelcs108@gmail.com>
2024-01-24 23:42:44 -0500

    New colorscheme

--- lua/configuration/lazy/colorscheme.lua
+++ lua/configuration/lazy/colorscheme.lua
@@ -1,12 +1,8 @@
 return {
-    "kvrohit/rasmus.nvim",
-    config = function()
-        vim.g.rasmus_bold_functions = true
-        vim.g.rasmus_bold_keywords = true
-        vim.g.rasmus_bold_booleans = true
-        vim.g.rasmus_bold_variables = true
-        vim.g.rasmus_transparent = true
-        vim.cmd.colorscheme("rasmus")
-    end
+   "AlessandroYorba/Alduin",
+   config = function()
+       vim.g.alduin_Shout_Become_Ethereal = 1
+       vim.cmd.colorscheme("alduin")
+   end
 }
 
