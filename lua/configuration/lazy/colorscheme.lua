d10ce0b2aa8cfc51151faf77885b82a8b9d1eff0
Abhay Patel <apatelcs108@gmail.com>
2024-05-28 09:57:53 -0400

    Found my favorite colorscheme

--- lua/configuration/lazy/colorscheme.lua
+++ lua/configuration/lazy/colorscheme.lua
@@ -1,17 +1,37 @@
 return {
-    "Yagua/nebulous.nvim",
+    -- "Yagua/nebulous.nvim",
+    -- config = function()
+    --     require("nebulous").setup({
+    --         variant = "nova",
+    --         disable = {
+    --             background = true,
+    --         },
+    --         italic = {
+    --             keywords = false,
+    --             variables = false,
+    --         },
+    --     })
+    --     vim.cmd.colorscheme("nebulous")
+    -- end
+    "ramojus/mellifluous.nvim",
     config = function()
-        require("nebulous").setup({
-            variant = "nova",
-            disable = {
-                background = true,
+        require("mellifluous").setup({
+            color_set = "alduin",
+            styles = {
+                comments = { italic = true },
+                keywords = { bold = true },
+                types = { bold = true },
             },
-            italic = {
-                keywords = false,
-                variables = false,
+            transparent_background = {
+                enabled = true,
+                floating_windows = true,
+                telescope = true,
+                file_tree = true,
+                cursor_line = true,
+                status_line = true,
             },
         })
-        vim.cmd.colorscheme("nebulous")
+        vim.cmd.colorscheme("mellifluous")
     end
 }
 
