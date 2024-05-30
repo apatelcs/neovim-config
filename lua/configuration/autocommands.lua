b7c80f581c0ff31329b6b780a43ef41f0faa54b5
Abhay Patel <apatelcs108@gmail.com>
2024-05-30 10:45:15 -0400

    Added AMAZING autocommand to remove trailing whitespace on write

--- lua/configuration/autocommands.lua
+++ lua/configuration/autocommands.lua
@@ -1,5 +1,4 @@
 vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
   group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
   callback = function()
     vim.highlight.on_yank()
@@ -19,5 +18,14 @@ vim.api.nvim_create_autocmd("LspAttach", {
         vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
         vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
     end
+})
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+    pattern = {"*"},
+    callback = function()
+      local save_cursor = vim.fn.getpos(".")
+      pcall(function() vim.cmd [[%s/\s\+$//e]] end)
+      vim.fn.setpos(".", save_cursor)
+    end,
 })
 
