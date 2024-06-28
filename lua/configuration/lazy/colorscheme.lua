7e71b8a021b29e24aad95f1a7cf7cc417232986a
Abhay Patel <apatelcs108@gmail.com>
2024-06-28 01:32:09 -0400

    Landed on catppuccin as colorscheme

--- lua/configuration/lazy/colorscheme.lua
+++ lua/configuration/lazy/colorscheme.lua
@@ -1,9 +1,49 @@
 return {
-    "Mofiqul/adwaita.nvim",
+    "catppuccin/nvim",
+    priority = 1000,
     config = function()
-        vim.g.adwaita_darker = true
-        vim.g.adwaita_disable_cursorline = true
-        vim.cmd.colorscheme("adwaita")
+        require("catppuccin").setup({
+            flavour = "mocha",
+            background = { dark = "mocha" },
+            transparent_background = false,
+            show_end_of_buffer = false,
+            term_colors = false,
+            styles = {
+                comments = { "italic" },
+                keywords = { "bold" },
+                booleans = { "bold" },
+                types = { "bold" },
+            },
+            integrations = {
+                cmp = true,
+                gitsigns = true,
+                treesitter = true,
+                native_lsp = {
+                    enabled = true,
+                    virtual_text = {
+                        errors = { "italic" },
+                        hints = { "italic" },
+                        warnings = { "italic" },
+                        information = { "italic" },
+                        ok = { "italic" },
+                    },
+                    underlines = {
+                        errors = { "underline" },
+                        hints = { "underline" },
+                        warnings = { "underline" },
+                        information = { "underline" },
+                        ok = { "underline" },
+                    },
+                    inlay_hints = {
+                        background = true,
+                    },
+                },
+                telescope = {
+                    enabled = true,
+                },
+            },
+        })
+        vim.cmd.colorscheme("catppuccin")
     end
 }
 
