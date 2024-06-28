5abc7020b4316482b51208c5f1d60249c21abb35
Abhay Patel <apatelcs108@gmail.com>
2024-06-28 19:21:29 -0400

    ... another new colorscheme

--- lua/configuration/lazy/colorscheme.lua
+++ lua/configuration/lazy/colorscheme.lua
@@ -1,49 +1,67 @@
+-- return {
+--     "catppuccin/nvim",
+--     priority = 1000,
+--     config = function()
+--         require("catppuccin").setup({
+--             flavour = "mocha",
+--             background = { dark = "mocha" },
+--             transparent_background = false,
+--             show_end_of_buffer = false,
+--             term_colors = false,
+--             styles = {
+--                 comments = { "italic" },
+--                 keywords = { "bold" },
+--                 booleans = { "bold" },
+--                 types = { "bold" },
+--             },
+--             integrations = {
+--                 cmp = true,
+--                 gitsigns = true,
+--                 treesitter = true,
+--                 native_lsp = {
+--                     enabled = true,
+--                     virtual_text = {
+--                         errors = { "italic" },
+--                         hints = { "italic" },
+--                         warnings = { "italic" },
+--                         information = { "italic" },
+--                         ok = { "italic" },
+--                     },
+--                     underlines = {
+--                         errors = { "underline" },
+--                         hints = { "underline" },
+--                         warnings = { "underline" },
+--                         information = { "underline" },
+--                         ok = { "underline" },
+--                     },
+--                     inlay_hints = {
+--                         background = true,
+--                     },
+--                 },
+--                 telescope = {
+--                     enabled = true,
+--                 },
+--             },
+--         })
+--         vim.cmd.colorscheme("catppuccin")
+--     end
+-- }
+
 return {
-    "catppuccin/nvim",
+    "folke/tokyonight.nvim",
+    lazy = false,
     priority = 1000,
     config = function()
-        require("catppuccin").setup({
-            flavour = "mocha",
-            background = { dark = "mocha" },
-            transparent_background = false,
-            show_end_of_buffer = false,
-            term_colors = false,
+        require("tokyonight").setup({
+            style = "moon",
+            transparent = false,
+            terminal_colors = true,
             styles = {
-                comments = { "italic" },
-                keywords = { "bold" },
-                booleans = { "bold" },
-                types = { "bold" },
-            },
-            integrations = {
-                cmp = true,
-                gitsigns = true,
-                treesitter = true,
-                native_lsp = {
-                    enabled = true,
-                    virtual_text = {
-                        errors = { "italic" },
-                        hints = { "italic" },
-                        warnings = { "italic" },
-                        information = { "italic" },
-                        ok = { "italic" },
-                    },
-                    underlines = {
-                        errors = { "underline" },
-                        hints = { "underline" },
-                        warnings = { "underline" },
-                        information = { "underline" },
-                        ok = { "underline" },
-                    },
-                    inlay_hints = {
-                        background = true,
-                    },
-                },
-                telescope = {
-                    enabled = true,
-                },
+                comments = { italic = true },
+                keywords = { italic = false, bold = true },
             },
         })
-        vim.cmd.colorscheme("catppuccin")
+        vim.cmd.colorscheme("tokyonight")
     end
 }
 
