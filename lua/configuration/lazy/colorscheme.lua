a0f4f0aecc2fc6f215a7957499508589387a0078
Abhay Patel <apatelcs108@gmail.com>
2024-07-20 12:53:12 -0400

    Using default vscode colorscheme

--- lua/configuration/lazy/colorscheme.lua
+++ lua/configuration/lazy/colorscheme.lua
@@ -1,18 +1,9 @@
 return {
-    "folke/tokyonight.nvim",
-    lazy = false,
-    priority = 1000,
+    "Mofiqul/vscode.nvim",
     config = function()
-        require("tokyonight").setup({
-            style = "moon",
-            transparent = false,
-            terminal_colors = true,
-            styles = {
-                comments = { italic = true },
-                keywords = { italic = false, bold = true },
-            },
+        require("vscode").setup({
+            style = "dark",
         })
-        vim.cmd.colorscheme("tokyonight")
+        vim.cmd.colorscheme("vscode")
     end
 }
-
