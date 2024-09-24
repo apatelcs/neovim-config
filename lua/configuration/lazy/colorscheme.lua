8f0cbd7fa85fb56d73dd9128d8de9b321806bc72
Abhay Patel <apatelcs108@gmail.com>
2024-09-24 13:23:04 -0400

    update

--- lua/configuration/lazy/colorscheme.lua
+++ lua/configuration/lazy/colorscheme.lua
@@ -1,9 +1,23 @@
+-- return {
+--     "Mofiqul/vscode.nvim",
+--     config = function()
+--         require("vscode").setup({
+--             style = "dark",
+--         })
+--         vim.cmd.colorscheme("vscode")
+--     end
+-- }
+
 return {
-    "Mofiqul/vscode.nvim",
+    "ellisonleao/gruvbox.nvim",
+    priority = 1000,
     config = function()
-        require("vscode").setup({
-            style = "dark",
+        require("gruvbox").setup({
+            bold = true,
+            overrides = {
+                SignColumn = {bg = "#282828"}
+            }
         })
-        vim.cmd.colorscheme("vscode")
+        vim.cmd.colorscheme("gruvbox")
     end
 }
