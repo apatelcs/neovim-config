e55aaedd608de5d289b33a1b4ec7c0c381dd2d79
Abhay Patel <apatelcs108@gmail.com>
2025-04-08 23:55:14 -0400

    added neo-tree, removed fugitive

--- lua/configuration/lazy/lsp.lua
+++ lua/configuration/lazy/lsp.lua
@@ -25,7 +25,6 @@ local servers = {
             },
         },
     },
-    tsserver = {},
     html = {},
     cssls = {},
     tailwindcss = {},
--- lua/configuration/lazy/treesitter.lua
+++ lua/configuration/lazy/treesitter.lua
@@ -1,22 +1,3 @@
--- return {
---     "nvim-treesitter/nvim-treesitter",
---     dependencies = {
---         "nvim-treesitter/nvim-treesitter-textobjects"
---     },
---     build = ":TSUpdate",
---     config = function()
---         require("nvim-treesitter.configs").setup({
---             ensure_installed = { "c", "lua", "rust", "python", "go" },
---             sync_install = false,
---             auto_install = true,
---             highlight = {
---                 enable = true,
---                 additional_vim_regex_highlighting = false,
---             }
---         })
---     end,
--- }
-
 return {
   "nvim-treesitter/nvim-treesitter",
   event = { "BufReadPre", "BufNewFile" },
