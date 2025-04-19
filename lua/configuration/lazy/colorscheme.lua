0880a731777f40faab00f32cb952de7f8758ad07
Abhay Patel <apatelcs108@gmail.com>
2025-04-19 11:04:39 -0400

    back to gruvbox

--- lua/configuration/lazy/colorscheme.lua
+++ lua/configuration/lazy/colorscheme.lua
@@ -1,10 +1,24 @@
 return {
-    "Mofiqul/vscode.nvim",
+    "ellisonleao/gruvbox.nvim",
+    priority = 1000,
     config = function()
-        require("vscode").setup({
-            transparent = true,
-            style = "dark",
+        require("gruvbox").setup({
+            terminal_colors = true,
+            undercurl = false,
+            underline = false,
+            bold = true,
+            italic = {
+                strings = true,
+                emphasis = true,
+                comments = true,
+                operators = false,
+                folds = true,
+            },
+            strikethrough = true,
+            inverse = true,
+            contrast = "hard",
+            transparent_mode = true,
         })
-        vim.cmd.colorscheme("vscode")
+        vim.cmd.colorscheme("gruvbox")
     end
 }
