49541560a88c66411035f5159dd388746237e071
Abhay Patel <apatelcs108@gmail.com>
2026-04-30 16:08:34 -0400

    using zenburned

--- lua/core/autocmds.lua
+++ lua/core/autocmds.lua
@@ -6,3 +6,16 @@ vim.api.nvim_create_autocmd("TextYankPost", {
     vim.highlight.on_yank()
   end,
 })
+
+vim.api.nvim_create_autocmd("ColorScheme", {
+  group = vim.api.nvim_create_augroup("custom_highlights", {}),
+  pattern = "zen*",
+  callback = function()
+    vim.api.nvim_set_hl(0, "Keyword", { bold = true })
+    vim.api.nvim_set_hl(0, "@keyword", { bold = true })
+    vim.api.nvim_set_hl(0, "Boolean", { bold = true })
+    vim.api.nvim_set_hl(0, "@boolean", { bold = true })
+    vim.api.nvim_set_hl(0, "@keyword.conditional", { bold = true })
+    vim.api.nvim_set_hl(0, "@keyword.repeat", { bold = true })
+  end,
+})
--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,24 +1,15 @@
 return {
-  "vague-theme/vague.nvim",
+  "zenbones-theme/zenbones.nvim",
+  dependencies = "rktjmp/lush.nvim",
   lazy = false,
   priority = 1000,
   config = function()
-    require("vague").setup({
-      -- optional configuration here
-      transparent = true,
-      italic = false,
-      colors = {
-        func = "#bc96b0",
-        keyword = "#787bab",
-        string = "#d4bd98",
-        -- string = "#8a739a",
-        -- string = "#f2e6ff",
-        -- number = "#f2e6ff",
-        -- string = "#d8d5b1",
-        number = "#8f729e",
-        -- type = "#dcaed7",
-      },
-    })
-    vim.cmd.colorscheme("vague")
+    vim.g.zenburned = {
+      solid_line_nr = false,
+      solid_float_border = true,
+      italic_strings = false,
+      transparent_background = true,
+    }
+    vim.cmd.colorscheme("zenburned")
   end,
 }
