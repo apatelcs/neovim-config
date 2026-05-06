3691072a83af0c1aa3f0c32dde339b7bac27c5d9
Abhay Patel <apatelcs108@gmail.com>
2026-05-06 14:09:07 -0400

    use gruvbox

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,15 +1,10 @@
 return {
-  "zenbones-theme/zenbones.nvim",
-  dependencies = "rktjmp/lush.nvim",
+  "sainnhe/gruvbox-material",
   lazy = false,
   priority = 1000,
   config = function()
-    vim.g.zenburned = {
-      solid_line_nr = false,
-      solid_float_border = true,
-      italic_strings = false,
-      transparent_background = true,
-    }
-    vim.cmd.colorscheme("zenburned")
-  end,
+    vim.g.gruvbox_material_transparent_background = 1
+    vim.g.gruvbox_material_enable_bold = 1
+    vim.cmd.colorscheme('gruvbox-material')
+  end
 }
