5cabeb8e9cf606d844868f93edca71ee949a0339
Abhay Patel <apatelcs108@gmail.com>
2026-05-17 10:50:30 -0400

    show hidden in oil

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -3,7 +3,13 @@ return {
 	dependencies = { { "nvim-mini/mini.icons", opts = {} } },
 	lazy = false,
 	config = function()
-		require("oil").setup()
+		require("oil").setup({
+			columns = { "icon" },
+			view_options = {
+				show_hidden = true,
+			},
+		})
 		vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+		vim.keymap.set("n", "<leader>-", require('oil').toggle_float, { desc = "Open parent directory" })
 	end,
 }
