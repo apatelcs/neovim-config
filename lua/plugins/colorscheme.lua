92660186558062eab431d4ea041aff64a0354cd7
Abhay Patel <apatelcs108@gmail.com>
2026-05-20 12:16:14 -0400

    using kanagawa now

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,10 +1,24 @@
 return {
-  "sainnhe/gruvbox-material",
+  "rebelot/kanagawa.nvim",
   lazy = false,
   priority = 1000,
   config = function()
-    vim.g.gruvbox_material_transparent_background = 1
-    vim.g.gruvbox_material_enable_bold = 1
-    vim.cmd.colorscheme('gruvbox-material')
-  end
+    require("kanagawa").setup({
+      undercurl = false,
+      commentStyle = { italic = true },
+      functionStyle = { italic = true, bold = true },
+      keywordStyle = { italic = false, bold = true },
+      transparent = true,
+      colors = {
+        theme = {
+          all = {
+            ui = {
+              bg_gutter = "none",
+            },
+          },
+        },
+      },
+    })
+    vim.cmd.colorscheme("kanagawa")
+  end,
 }
