c633cef0949db47a961ea6400773c002e61bce8e
Abhay Patel <apatelcs108@gmail.com>
2026-06-04 13:32:14 -0400

    small update

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -4,7 +4,7 @@ return {
   priority = 1000,
   config = function()
     require("catppuccin").setup({
-      flavour = "latte",
+      flavour = "mocha",
       transparent_background = true,
       float = {
         transparent = true,
